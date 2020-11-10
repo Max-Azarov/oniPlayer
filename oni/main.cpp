76509c2ef99cf02f4d098c17ed246fab909e2528
Max Azarov <a2708457@yandex.ru>
2020-11-10 08:57:30 +0500

    Before disadvantage were fixed

--- oni/main.cpp
+++ oni/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
     MainWindow w;
     w.setWindowFlags(Qt::MSWindowsFixedSizeDialogHint|Qt::WindowMinimizeButtonHint|Qt::WindowCloseButtonHint);
     w.setGeometry(30, 30, 600, 100);
